213e25a8c6dc766f6f611c71a34c2d912bb2ea06
shitaro <apprentice.physicist.shitaro@gmail.com>
2019-08-16 16:10:04 +0900

    Hello world with Device Code

--- example/hello.cu
+++ example/hello.cu
@@ -1,13 +1,19 @@
+#include <iostream>
 #include <stdio.h>
 
-__global__
+/* device function */
+/* device function processed by NVIDIA compiler */
+__global__ // Runs on the device, called from host code
 void cuda_hello(void) {
     printf("Hello World from GPU!\n");
 }
 
 int main(void)
 {
-    cuda_hello<<<1,1000>>>();
+    std::cout << "Hello World from Host" << std::endl;
+
+    cuda_hello<<<1,1000>>>(); // kernel launch: triple angle bracets mark a call from host code to device code
+
     cudaDeviceSynchronize();
     return 0;
 }
